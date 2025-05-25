75a83d34fa03a5f520198c28332c1ce9bfbda3c9
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-05-25 11:33:08 -0600

    Store more than one piece of data

--- src/main/java/org/hazelv/chime/ChordName.java
+++ src/main/java/org/hazelv/chime/ChordName.java
@@ -2,5 +2,5 @@ package org.hazelv.chime;
 public enum ChordName {
     START, START2, //verification
     ADD, SUBTRACT, MULTIPLY, DIVIDE, //mathematical operations
-    PRINT,
+    PRINT, STORE, GET
 }
--- src/main/java/org/hazelv/chime/Song.java
+++ src/main/java/org/hazelv/chime/Song.java
@@ -1,17 +1,19 @@
 package org.hazelv.chime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+
 import static org.hazelv.chime.NoteName.*;
 
 public class Song {
     public List<List<NoteName>> chords;
     public List<Object> code = new ArrayList<>();
+    public List<Stack<Float>> data = new ArrayList<>();
     public float currentValue = 0;
 
     public Song(List<List<NoteName>> chords) {
         this.chords = chords;
+        for (int i = 0; i < 128; i++) {
+            data.add(new Stack<>());
+        }
     }
 
     public void parse() {
@@ -30,6 +32,10 @@ public class Song {
                 code.add(ChordName.DIVIDE);
             } else if (Objects.equals(chord, new ArrayList<>(Arrays.asList(A5, Db6, E6)))) {
                 code.add(ChordName.PRINT);
+            } else if (Objects.equals(chord, new ArrayList<>(Arrays.asList(B5, Eb6, Gb6)))) {
+                code.add(ChordName.STORE);
+            } else if (Objects.equals(chord, new ArrayList<>(Arrays.asList(Bb5, D6, F6)))) {
+                code.add(ChordName.GET);
             } else if (chord.size() == 1) {
                 code.add(chord.getFirst().ordinal());
             }
@@ -41,7 +47,7 @@ public class Song {
             System.out.println(chords);
             throw new IllegalArgumentException("Invalid start sequence. (Are you sure this is a program?)");
         }
-        System.out.println(code);
+        //System.out.println(code);
         for (int i = 0; i < (code.size() - 1); i++) {
             Object instruction = code.get(i);
             if (instruction.equals(ChordName.START) || instruction.equals(ChordName.START2)) {
@@ -53,7 +59,7 @@ public class Song {
             } else if (instruction instanceof Integer) {
                 throw new IllegalArgumentException("Integer literal before instruction at chord: " + i);
             }
-            int[] arguments = getArguments(i);
+            float[] arguments = getArguments(i);
             switch (instruction) {
                 case ChordName.ADD:
                     currentValue = arguments[0] + arguments[1];
@@ -69,11 +75,19 @@ public class Song {
                     i = i + 2;
                     break;
                 case ChordName.DIVIDE:
-                    currentValue = (float)arguments[0] / (float)arguments[1];
+                    currentValue = arguments[0] / arguments[1];
                     i = i + 2;
                     break;
                 case ChordName.PRINT:
                     System.out.println(currentValue);
+                    break;
+                case ChordName.STORE:
+                    data.get((int) arguments[0]).push(arguments[0]);
+                    i++;
+                    break;
+                case ChordName.GET:
+                    currentValue = data.get((int) arguments[0]).pop();
+                    i++;
                     break;
                 default:
                     throw new IllegalStateException("Unknown Instruction: " + instruction + " at chord: " + i);
@@ -87,7 +101,7 @@ public class Song {
         return chords.toString();
     }
 
-    public int[] getArguments(int index) {
-        return new int[]{(int)code.get(index + 1), (int)code.get(index + 2)};
+    public float[] getArguments(int index) {
+        return new float[]{(float)code.get(index + 1), (float)code.get(index + 2)};
     }
 }
